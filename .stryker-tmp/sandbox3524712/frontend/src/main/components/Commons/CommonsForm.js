561a092264c61bf170c0523e807a6d918d6bc952
Zoey <zoey@Zoeys-MacBook-Pro.local>
2022-05-31 18:33:43 -0700

    xz-rename showleaderboard to leaderboard

---
+++ .stryker-tmp/sandbox3524712/frontend/src/main/components/Commons/CommonsForm.js
@@ -0,0 +1,127 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
+function CommonsForm({ initialCommons, submitAction, buttonLabel = "Create" }) {
+  // Stryker disable all
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm(
+    { defaultValues: initialCommons || {} }
+  );
+  // Stryker enable all
+
+  const testid = "CommonsForm";
+
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      {initialCommons && (
+        <Form.Group className="mb-3" >
+          <Form.Label htmlFor="id">Id</Form.Label>
+          <Form.Control
+            data-testid={`${testid}-id`}
+            id="id"
+            type="text"
+            {...register("id")}
+            value={initialCommons.id}
+            disabled
+          />
+        </Form.Group>
+      )}
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="name">Commons Name</Form.Label>
+        <Form.Control
+          data-testid={`${testid}-name`}
+          id="name"
+          type="text"
+          isInvalid={!!errors.name}
+          {...register("name", { required: "Commons name is required" })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.name?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="startingBalance">Starting Balance</Form.Label>
+        <Form.Control
+          id="startingBalance"
+          data-testid={`${testid}-startingBalance`}
+          type="number"
+          step="0.01"
+          isInvalid={!!errors.startingBalance}
+          {...register("startingBalance", {
+            valueAsNumber: true,
+            required: "Starting Balance is required",
+            min: { value: 0.01, message: "Starting Balance must be positive" },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.startingBalance?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="cowPrice">Cow Price</Form.Label>
+        <Form.Control
+          data-testid={`${testid}-cowPrice`}
+          id="cowPrice"
+          type="number"
+          step="0.01"
+          isInvalid={!!errors.cowPrice}
+          {...register("cowPrice", {
+            valueAsNumber: true,
+            required: "Cow price is required",
+            min: { value: 0.01, message: "Cow price must be positive" },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.cowPrice?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="milkPrice">Milk Price</Form.Label>
+        <Form.Control
+          data-testid={`${testid}-milkPrice`}
+          id="milkPrice"
+          type="number"
+          step="0.01"
+          isInvalid={!!errors.milkPrice}
+          {...register("milkPrice", {
+            valueAsNumber: true,
+            required: "Milk price is required",
+            min: { value: 0.01, message: "Milk price must be positive" },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.milkPrice?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="startingDate">Starting Date</Form.Label>
+        <Form.Control
+          data-testid={`${testid}-startingDate`}
+          id="startingDate"
+          type="date"
+          isInvalid={!!errors.startingDate}
+          {...register("startingDate", {
+            valueAsDate: true,
+            validate: {
+              isPresent: (v) => !isNaN(v) || "Starting date is required",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.startingDate?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Button type="submit" data-testid="CommonsForm-Submit-Button">{ buttonLabel }</Button>
+    </Form>
+  );
+}
+
+export default CommonsForm;
