ae2a32e1f7e361b60c387f6597e6889f149be845
Heather Nguyen <heathernguyen@ResNet-65-122.resnet.ucsb.edu>
2022-05-31 17:42:42 -0700

    hn - commonstables test added showLeaderboard

--- frontend/src/tests/components/Commons/CommonsTable.test.js
+++ frontend/src/tests/components/Commons/CommonsTable.test.js
@@ -65,8 +65,8 @@ describe("UserTable tests", () => {
 
     );
 
-    const expectedHeaders = ["id", "Name", "Cow Price", 'Milk Price', 'Starting Balance', 'Starting Date'];
-    const expectedFields = ["id", "name", "cowPrice", "milkPrice", "startingBalance", "startingDate"];
+    const expectedHeaders = ["id", "Name", "Cow Price", 'Milk Price', 'Starting Balance', 'Show Leaderboard', 'Starting Date'];
+    const expectedFields = ["id", "name", "cowPrice", "milkPrice", "startingBalance", "showLeaderboard","startingDate"];
     const testId = "CommonsTable";
 
     expectedHeaders.forEach((headerText) => {
