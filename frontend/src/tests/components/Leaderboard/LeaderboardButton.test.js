f2bc67b466f50fd35e70976fb7907e956ae405d4
Peter Brede <peterbrede@ucsb.edu>
2022-05-31 23:13:19 -0700

    pb - fixed leaderboardButton tests

--- frontend/src/tests/components/Leaderboard/LeaderboardButton.test.js
+++ frontend/src/tests/components/Leaderboard/LeaderboardButton.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import LeaderboardButton from "main/components/Leaderboard/LeaderboardButton"; 
 
 describe("LeaderboardButton tests", () => {
@@ -6,6 +6,18 @@ describe("LeaderboardButton tests", () => {
         render(
             <LeaderboardButton />
         );
+    });
+
+    test("clicking LeaderboardButton", async () => {
+        render(
+            <LeaderboardButton />
+        );
+
+        const leaderboardButton = screen.getByTestId("leaderboard-button");
+        const alertMock = jest.spyOn(window,'alert').mockImplementation(); 
+        fireEvent.click(leaderboardButton);
+        expect(alertMock).toHaveBeenCalledTimes(1);
+
     });
 
 });
