87a195e161ca25ac0992eb73078395c2e011a99b
Calvin Dougher <cwdougher@umail.ucsb.edu>
2022-06-03 15:04:41 -0700

    cd - Further testing button navigation

--- frontend/src/main/components/Commons/LeaderboardPlayPageButton.js
+++ frontend/src/main/components/Commons/LeaderboardPlayPageButton.js
@@ -1,34 +1,37 @@
 import React from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
-import leaderboardIcon from "./../../../assets/leaderboard.png"; 
-import {Link} from 'react-router-dom'
+import leaderboardIcon from "./../../../assets/leaderboard.png";
+import { Link } from "react-router-dom";
 
-const Leaderboard = () =>  {
-    
-    return (
-        <Card>
-        <Card.Header as="h5">Leaderboard</Card.Header>
-        <Card.Body>      
-            <Card.Title></Card.Title>
-                <Row>
-                    <Col>
-                        <Card.Text>
-                            <img alt="Leaderoard Icon" width={100} height={100} className="icon" src={leaderboardIcon}></img>
-                        </Card.Text>
-                    </Col>
-                    <Col>
-                        <div>
-                        <Link to='/leaderboard/'>
-                        <Button data-testid={"leaderboard-playpage-button"}>Board</Button>
-                        </Link>
-                        </div>
-                    </Col>
-                </Row>
-                    Click to view leaderboard 
-        </Card.Body>
-        </Card>
-    ); 
+const Leaderboard = () => {
+  return (
+    <Card>
+      <Card.Header as="h5">Leaderboard</Card.Header>
+      <Card.Body>
+        <Card.Title></Card.Title>
+        <Row>
+          <Col>
+            <Card.Text>
+              <img
+                alt="Leaderoard Icon"
+                width={100}
+                height={100}
+                className="icon"
+                src={leaderboardIcon}
+              ></img>
+            </Card.Text>
+          </Col>
+          <Col>
+            <div>
+              <Link to="/leaderboard/">
+                <Button data-testid={"leaderboard-button"}>Show</Button>
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+};
 
-}; 
-
-export default Leaderboard; 
+export default Leaderboard;
--- frontend/src/tests/components/Commons/LeaderboardPlayPageButton.test.js
+++ frontend/src/tests/components/Commons/LeaderboardPlayPageButton.test.js
@@ -1,34 +1,31 @@
-import { render, screen } from "@testing-library/react";
-import LeaderboardPlayPage from "main/components/Commons/LeaderboardPlayPageButton"; 
+import { fireEvent, render, screen } from "@testing-library/react";
+import LeaderboardPlayPage from "main/components/Commons/LeaderboardPlayPageButton";
 import { MemoryRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-
 describe("LeaderboardPlayPage tests", () => {
-    const queryClient = new QueryClient();
-    test("renders without crashing", () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <LeaderboardPlayPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-    });
+  const queryClient = new QueryClient();
+  test("renders without crashing", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <LeaderboardPlayPage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+  });
 
-    test("clicking LeaderboardPlayPage", async () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <LeaderboardPlayPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
+  test("clicking LeaderboardButton", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <LeaderboardPlayPage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
 
-        expect(await screen.findByText("Board")).toBeInTheDocument();
-        const PlayPageMenu = screen.getByTestId("leaderboard-playpage-button");
-        expect(PlayPageMenu).toBeInTheDocument();
-
-    });
-
-}); 
+    expect(await screen.findByTestId("leaderboard-button")).toBeInTheDocument();
+    const LeaderboardButton = screen.getByTestId("leaderboard-button");
+    fireEvent.click(LeaderboardButton);
+  });
+});
