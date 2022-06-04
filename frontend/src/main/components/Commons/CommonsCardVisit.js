8b7ab0fee0d1d225f2db709fc3174c75f9cc1e76
Calvin Dougher <cwdougher@umail.ucsb.edu>
2022-06-03 20:23:30 -0700

    cd - Tests Pass Mutation Coverage Not 100

--- frontend/src/main/components/Commons/CommonsCardVisit.js
+++ frontend/src/main/components/Commons/CommonsCardVisit.js
@@ -3,62 +3,69 @@ import { Card, Button, Container, Row, Col } from "react-bootstrap";
 
 import { useBackendMutation } from "main/utils/useBackend";
 
-const CommonsCardVisit = ({ buttonText, buttonLink,buttonText1, commons, user}) => {
+const CommonsCardVisit = ({
+  buttonText,
+  buttonLink,
+  buttonText1,
+  commons,
+  user,
+}) => {
+  let cId = commons.id;
+  let uId = user;
 
-    let cId = commons.id;
-    let uId = user;
+  const objectToAxiosParams = () => ({
+    url: `/api/commons/${cId}/users/${uId}`,
+    method: "DELETE",
+  });
 
-    const objectToAxiosParams = ( ) => ({
-        url: `/api/commons/${cId}/users/${uId}`,
-        method: "DELETE",
-        
-    });
-    
-    const mutation = useBackendMutation(
+  const mutation = useBackendMutation(objectToAxiosParams, {}, [
+    "/api/currentUser",
+  ]);
 
-        objectToAxiosParams,
-        {},
-        ["/api/currentUser"],
-    );
+  const deleteCallback = async () => {
+    mutation.mutate();
+  };
 
-    const deleteCallback = async () => {mutation.mutate() ;}
+  return (
+    <Card.Body
+      style={
+        // Stryker disable next-line all : don't mutation test CSS
+        { fontSize: "20px", borderTop: "1px solid lightgrey" }
+      }
+    >
+      <Container>
+        <Row>
+          <Col sx={4} data-testid="commonsCardVisit-id">
+            {commons.id}
+          </Col>
+          <Col sx={4} data-testid="commonsCardVisit-name">
+            {commons.name}
+          </Col>
+          {buttonText != null && (
+            <Col sm={4}>
+              <Button
+                data-testid={`commonsCardVisit-button-${buttonText}-${commons.id}`}
+                size="sm"
+                className="mx-4"
+                onClick={() => buttonLink(commons.id)}
+              >
+                {buttonText}
+              </Button>
 
-    return (
-        <Card.Body style={
-            // Stryker disable next-line all : don't mutation test CSS 
-            { fontSize: "20px", borderTop: "1px solid lightgrey" }
-        }>
-            <Container>
-                <Row>
-                    <Col sx={4} data-testid="commonsCardVisit-id">{commons.id}</Col>
-                    <Col sx={4} data-testid="commonsCardVisit-name">{commons.name}</Col>
-                    {buttonText != null &&
-                        <Col sm={4}>
-                            <Button
-                                data-testid={`commonsCardVisit-button-${buttonText}-${commons.id}`}
-                                size="sm"
-                                className="mx-4"
-                                onClick={() => buttonLink(commons.id)} >{buttonText}
-
-
-                            </Button>
-
-                            <Button
-                                data-testid={`commonsCardVisit-button-${buttonText1}-${commons.id}`}
-                                size="sm"
-                                className="mx-4"
-                                onClick={ deleteCallback } >{buttonText1}
-
-                            </Button>
-
-                        </Col>
-                    }
-                </Row>
-            </Container>
-        </Card.Body>
-    );
+              <Button
+                data-testid={`commonsCardVisit-button-${buttonText1}-${commons.id}`}
+                size="sm"
+                className="mx-4"
+                onClick={deleteCallback}
+              >
+                {buttonText1}
+              </Button>
+            </Col>
+          )}
+        </Row>
+      </Container>
+    </Card.Body>
+  );
 };
-
-
 
 export default CommonsCardVisit;
--- frontend/src/tests/pages/HomePage.test.js
+++ frontend/src/tests/pages/HomePage.test.js
@@ -8,90 +8,133 @@ import HomePage from "main/pages/HomePage";
 import commonsFixtures from "fixtures/commonsFixtures";
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import { usersFixtures } from "fixtures/usersFixtures";
 import userCommonsFixtures from "fixtures/userCommonsFixtures";
 
-
 describe("HomePage tests", () => {
-    const queryClient = new QueryClient();
-    const axiosMock = new AxiosMockAdapter(axios);
-    apiCurrentUserFixtures.userOnly.user = userCommonsFixtures.oneUserCommons[0].user;
+  const queryClient = new QueryClient();
+  const axiosMock = new AxiosMockAdapter(axios);
+  apiCurrentUserFixtures.userOnly.user =
+    userCommonsFixtures.oneUserCommons[0].user;
 
+  beforeEach(() => {
+    axiosMock.reset();
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  });
 
-    beforeEach(() => {
-        axiosMock.reset();
-        axiosMock.resetHistory();
-        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    });
+  test("renders without crashing when lists return empty list", () => {
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet("/api/commons/all").reply(200, []);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
 
-    test("renders without crashing when lists return empty list", () => {
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock.onGet("/api/commons/all").reply(200, []);
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <HomePage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
+    const title = screen.getByTestId("homePage-title");
+    expect(title).toBeInTheDocument();
+    expect(typeof title.textContent).toBe("string");
+    expect(title.textContent).toEqual("Howdy Farmer");
+  });
 
-        const title = screen.getByTestId("homePage-title");
-        expect(title).toBeInTheDocument();
-        expect(typeof (title.textContent)).toBe('string');
-        expect(title.textContent).toEqual('Howdy Farmer');
-    });
+  test("renders without crashing when lists are full", () => {
+    apiCurrentUserFixtures.userOnly.user.commons = commonsFixtures.oneCommons;
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/commons/all")
+      .reply(200, commonsFixtures.threeCommons);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
 
-    test("renders without crashing when lists are full", () => {
-        apiCurrentUserFixtures.userOnly.user.commons = commonsFixtures.oneCommons;
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock.onGet("/api/commons/all").reply(200, commonsFixtures.threeCommons);
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <HomePage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
+    const title = screen.getByTestId("homePage-title");
+    expect(title).toBeInTheDocument();
+    expect(typeof title.textContent).toBe("string");
+    expect(title.textContent).toEqual("Howdy Farmer");
+  });
 
-        const title = screen.getByTestId("homePage-title");
-        expect(title).toBeInTheDocument();
-        expect(typeof (title.textContent)).toBe('string');
-        expect(title.textContent).toEqual('Howdy Farmer');
-    });
+  test("Redirects to the PlayPage when you click visit", async () => {
+    apiCurrentUserFixtures.userOnly.user.commons = commonsFixtures.oneCommons;
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/commons/all")
+      .reply(200, commonsFixtures.threeCommons);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
 
-    test("Redirects to the PlayPage when you click visit", async () => {
-        apiCurrentUserFixtures.userOnly.user.commons = commonsFixtures.oneCommons;
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock.onGet("/api/commons/all").reply(200, commonsFixtures.threeCommons);
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <HomePage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
+    expect(
+      await screen.findByTestId("commonsCardVisit-button-Visit-1")
+    ).toBeInTheDocument();
+    const visitButton = screen.getByTestId("commonsCardVisit-button-Visit-1");
+    fireEvent.click(visitButton);
+  });
 
-        expect(await screen.findByTestId("commonsCard-button-Visit-1")).toBeInTheDocument();
-        const visitButton = screen.getByTestId("commonsCard-button-Visit-1");
-        fireEvent.click(visitButton);
-    });
+  test("Calls the callback when you click unjoin", async () => {
+    apiCurrentUserFixtures.userOnly.user.commons = commonsFixtures.oneCommons;
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/commons/all")
+      .reply(200, commonsFixtures.threeCommons);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
 
-    test("Calls the callback when you click join", async () => {
-        apiCurrentUserFixtures.userOnly.user.commons = commonsFixtures.oneCommons;
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock.onGet("/api/commons/all").reply(200, commonsFixtures.threeCommons);
-        axiosMock.onPost("/api/commons/join").reply(200, commonsFixtures.threeCommons[0]);
+    expect(
+      await screen.findByTestId("commonsCardVisit-button-Unjoin-1")
+    ).toBeInTheDocument();
+    const unjoinButton = screen.getByTestId("commonsCardVisit-button-Unjoin-1");
+    fireEvent.click(unjoinButton);
+  });
 
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <HomePage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
+  test("Calls the callback when you click join", async () => {
+    apiCurrentUserFixtures.userOnly.user.commons = commonsFixtures.oneCommons;
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/commons/all")
+      .reply(200, commonsFixtures.threeCommons);
+    axiosMock
+      .onPost("/api/commons/join")
+      .reply(200, commonsFixtures.threeCommons[0]);
 
-        expect(await screen.findByTestId("commonsCard-button-Join-1")).toBeInTheDocument();
-        const joinButton = screen.getByTestId("commonsCard-button-Join-1");
-        fireEvent.click(joinButton);
-    });
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+
+    expect(
+      await screen.findByTestId("commonsCard-button-Join-1")
+    ).toBeInTheDocument();
+    const joinButton = screen.getByTestId("commonsCard-button-Join-1");
+    fireEvent.click(joinButton);
+  });
 });
