12dc3aee31b307c2c9816cdcc880cebb50d1f982
Charanpreet  Singh <charanpreet@Charanpreets-MacBook-Pro.local>
2022-06-03 17:15:49 -0700

    Testing Code without Backend

--- frontend/src/main/pages/LeaderboardPage.js
+++ frontend/src/main/pages/LeaderboardPage.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 import LeaderboardTable from 'main/components/Leaderboard/LeaderboardTable';
 
 export default function LeaderboardPage() {
     const currentUser = useCurrentUser();
-    let { id } = useParams();  
-    // Stryker disable  all 
+    // let { id } = useParams();  
+    // // Stryker disable  all 
     const { data: leaderboard, error: _error, status: _status } =
     useBackend(
-        [`/api/leaderboard?id=${id}`],
-        { method: "GET", url: `/api/leaderboard/all`, params: {id} },
+        [],
+        {},
         []
     );
     // Stryker enable  all 
-
+     
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Leaderboard</h1>
         <LeaderboardTable leaderboard={leaderboard} currentUser={currentUser} />
+        {/* <LeaderboardTable /> */}
       </div>
     </BasicLayout>
   )
