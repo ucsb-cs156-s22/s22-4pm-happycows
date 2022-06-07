ac650e34f5ae4afb762dbc7f9ae91c8f0c691a96
Peter Brede <peterbrede@ucsb.edu>
2022-06-06 19:57:54 -0700

    combined branches for demo

--- frontend/src/main/components/Commons/CommonsOverview.js
+++ frontend/src/main/components/Commons/CommonsOverview.js
@@ -6,8 +6,8 @@ export default function CommonsOverview({ commons }) {
         <Card data-testid="CommonsOverview">
             <Card.Header as="h5">Announcements</Card.Header>
             <Card.Body>
-                <Card.Title>Today is day {commons.day}! This game will end on {commons.endDate}.</Card.Title>
-                <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
+                <Card.Title>Today is day 2{commons.day}! This game will end on 12/5/22{commons.endDate}.</Card.Title>
+                <Card.Text>Total Players: 1{commons.totalPlayers}</Card.Text>
             </Card.Body>
         </Card>
     );
